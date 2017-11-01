ba97d5943052c7faf8b64c2cbcfeb8cafec4e275
snetmonster <snetmonster@gmail.com>
2017-11-02 00:53:00 +0100

    Logger now displays the temperature in degrees

--- Rasp_Arduino_TempLogger/RPI/Logger.py
+++ Rasp_Arduino_TempLogger/RPI/Logger.py
@@ -1,7 +1,4 @@
 from bluepy.btle import Scanner, DefaultDelegate
-import matplotlib.pyplot as plt
-
-
 
 class ScanDelegate(DefaultDelegate):
     def __init__(self):
@@ -10,12 +7,24 @@ class ScanDelegate(DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             print("Discovered device", dev.addr)
+            print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType,     dev.rssi))
         elif isNewData:
             print("Received new data from", dev.addr)
-        print("Device %s (%s), RSSI=%d dB" % (dev.addr, dev.addrType,     dev.rssi))
+        name=""
+        val= 0     
+        isASensor = False
         for (adtype, desc, value) in dev.getScanData():
+            if(adtype == 22):
+                raw = dev.scanData.get(adtype, None)
+                # Convert nrf_Float to normal float
+                val=float(int.from_bytes(raw[2:4], byteorder='little', signed=False))/100.0;
+            if(adtype == 9):
+                name=value
+                if name.startswith("AN"):
+                    isASensor=True
         
-            print("%d  %s = %s" % (adtype,desc, value))
+        if isASensor == True:
+            print("{} = {}°".format(name, val))
 
 scanner = Scanner().withDelegate(ScanDelegate())
 devices = scanner.scan(0.0)
